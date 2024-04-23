3544c4bcdfbf0d9ca3a09a2599096b9a1f8968c9
damien <contact.damiendiaz@gmail.com>
2024-04-23 20:40:55 +0200

    css pages and js

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+const identity = {
+    firstName: "Damien",
+    country: "France",
+    occupation: "Developpeur web FullStack",
+    hobbies: ["sportif", "parresseux", "épicurien"],
+  };
+  
+  const skills = ["HTML", "CSS", "JavaScript", "React", "Astro", "Symfony"];
+
+  export { identity, skills };
